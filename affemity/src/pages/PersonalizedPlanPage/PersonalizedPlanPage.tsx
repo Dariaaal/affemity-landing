e641b0e4d9482a13c6c9eac0c393382ed119c4da
Dariaaal <111464521+Dariaaal@users.noreply.github.com>
2025-03-19 21:28:28 +0200

    remove unnecessary

--- affemity/src/pages/PersonalizedPlanPage/PersonalizedPlanPage.tsx
+++ affemity/src/pages/PersonalizedPlanPage/PersonalizedPlanPage.tsx
@@ -5,7 +5,6 @@ import { cx } from "../../lib/classNames";
 import css from "./PersonalizedPlanPage.module.css";
 import { Goal } from "../../models/Goal";
 import PopUp from "../../components/PopUp/PopUp";
-import { Comment } from "../../models/Comment";
 import ReviewsCarousel from "../../components/Reviews/ReviewsCarousel";
 
 const initialGoals: Goal[] = [
@@ -28,24 +27,6 @@ const initialGoals: Goal[] = [
     title: "Prioritizing challenges",
     progress: 0,
     question: "Are you open to self-improvement?",
-  },
-];
-
-const reviews: Comment[] = [
-  {
-    name: "Rosalee",
-    text: "I gotta say, this course is a game-changer! It’s chock-full of amazing sentences to send or tell to your guy, and the insights into the way men think and act are totally worth the pric...",
-    rating: 4.6,
-  },
-  {
-    name: "Clara",
-    text: "I’m totally digging this plan! It’s all about understanding men and their psychology, rather than just guessing what’s going on in their heads. And it’s not just about saying th...",
-    rating: 5.0,
-  },
-  {
-    name: "Stephanie",
-    text: "Okay, I gotta admit, I’m a bit shy when it comes to guys. But this course breaks things down into easy steps and gives you the confidence to put yourself out there. I even ...",
-    rating: 4.8,
   },
 ];
 
